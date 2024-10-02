cf681ef938a64e73e716b9b1970e58f7ff34a812
degorov <egorovdvrsreu@gmail.com>
2024-10-02 18:25:22 +0300

    basic db functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psycopg2
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from models import create_tables, Publisher, Book, Shop
+from models import create_tables, delete_all_tables, Publisher, Book, Shop, Stock, Sale
 
 # Получение данных для подключения к БД
 def enter_db_user_credentials():
@@ -13,6 +13,44 @@ def enter_db_user_credentials():
     password = str(input())   
     return db, user, password
 
+# Тестирование
+def testing():
+    knigi_v_dom = Publisher(name = 'Книги в каждый дом!')
+    knigi_12345 = Publisher(name = 'Книги РАЗ, ДВА, ТРИ, ЧЕТЫРЕ, ПЯТЬ')
+    kniga1 = Book(title = 'Собрание сочинений в 12 томах. Том 1. Пять недель на воздушном шаре. С земли на луну. Вокруг луны (сборник)',
+                  id_publisher = 1)
+    kniga2 = Book(title = 'Сказки, басни, песни и пляски', id_publisher = 2)
+    kniga3 = Book(title = 'Полное собрание сочинений Иванова', id_publisher = 2)
+    shop1 = Shop(name = 'Книжный на Пушкина')
+    shop2 = Shop(name = 'Книжки и пышки')
+    stock1 = Stock(id_book = 1, id_shop = 1, count = 10)
+    stock2 = Stock(id_book = 1, id_shop = 2, count = 999)
+    stock3 = Stock(id_book = 2, id_shop = 1, count = 30)
+    sale1 = Sale(price = 100.00, date_sale = '2015-12-29 12:00', id_stock = 1, count = 1)    
+    sale2 = Sale(price = 99.99, date_sale = '2016-05-10 12:34', id_stock = 2, count = 2)
+    sale3 = Sale(price = 123.99, date_sale = '205-12-30 08:30', id_stock = 3, count = 20)
+    session.add_all([knigi_v_dom, knigi_12345, kniga1, kniga2, kniga3, shop1, shop2, 
+                    stock1, stock2, stock3, sale1, sale2, sale3])
+    session.commit()
+    
+    q1 = session.query(Publisher).join(Book.publisher)
+    for pub in q1.all():
+        for b in pub.book:
+            sq1 = session.query(Stock).filter(Stock.id_book == b.id).subquery()
+            q2 = session.query(Sale).join(sq1, Sale.id_stock == sq1.c.id)
+            for s in q2.all():
+                print(b.title, ' - ', pub.name, ' - ', s.price, ' - ', s.date_sale)
+
+
+    # q2 = session.query(Shop).join(Stock.shop)
+    # # for sho in subq2.all():
+    # #     for sto in sho.stock:
+    # #         print(sho.id, sho.name, ' - ', sto.id, sto.count, 'шт.')
+    # # subq3 = subq1.join(subq2, subq1.c.id == subq2.c.id_book)
+
+    # subquery1 = q1.subquery()
+    # subquery2 = q2.subquery()
+
 #Подключение к БД, создание сессии
 db, user, password = enter_db_user_credentials()
 DSN = f'postgresql://{user}:{password}@localhost:5432/{db}'
@@ -20,14 +58,12 @@ engine = sqlalchemy.create_engine(DSN)
 Session = sessionmaker(bind = engine)
 session = Session()
 
+#УДАЛЕНИЕ ВСЕХ ТАБЛИЦ ИЗ БД - для удобства тестирования
+delete_all_tables(engine)
+#создание таблиц
 create_tables(engine)
 
-knigi_v_dom = Book(title = 'Книги в каждый дом!')
-print(knigi_v_dom.id)
+testing()
 
-session.add(knigi_v_dom)
 session.commit()
-
-print('id издателя:' + knigi_v_dom.id)
-
 session.close()
--- models.py
+++ models.py
@@ -10,6 +10,9 @@ class Publisher(Base):
     name = sq.Column(sq.String(length=100), unique=True)
     #т.к., к примеру, для отчетов ПФР в названиях организаций ограничение 100 символов
 
+    # def __str__(self):
+    #     return f'{self.id}: {self.name}'
+
 class Book(Base):
     __tablename__ = "book"
     id = sq.Column(sq.Integer, primary_key=True)
@@ -26,12 +29,28 @@ class Shop(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True)
 
-# class Stock(Base):
-#     __tablename__ = "stock"
+class Stock(Base):
+    __tablename__ = "stock"
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer, nullable=False, default=0)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable = False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable = False)
 
+    book = relationship(Book, backref="stock")
+    shop = relationship(Shop, backref="stock")
 
-# class Sale(Base):
-#     __tablename__ = "sale"
+class Sale(Base):
+    __tablename__ = "sale"
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column(sq.DateTime, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable = False)
+    count = sq.Column(sq.Integer, nullable=False)
+
+    stock = relationship(Stock, backref="sale")
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
+
+def delete_all_tables(engine):
+    Base.metadata.drop_all(engine)
