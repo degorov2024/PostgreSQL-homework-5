dbc1ce3e268d1f52f4074bbb3f8177b4f3524760
degorov <egorovdvrsreu@gmail.com>
2024-10-03 16:44:26 +0300

    informational queries done

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def enter_db_user_credentials():
     password = str(input())   
     return db, user, password
 
-# Тестирование
-def testing():
+# Наполнение таблиц данными
+def import_data_to_bd():
     knigi_v_dom = Publisher(name = 'Книги в каждый дом!')
     knigi_12345 = Publisher(name = 'Книги РАЗ, ДВА, ТРИ, ЧЕТЫРЕ, ПЯТЬ')
-    kniga1 = Book(title = 'Собрание сочинений в 12 томах. Том 1. Пять недель на воздушном шаре. С земли на луну. Вокруг луны (сборник)',
+    kniga1 = Book(title = 'Турбо Паскаль за две недели!',
                   id_publisher = 1)
     kniga2 = Book(title = 'Сказки, басни, песни и пляски', id_publisher = 2)
     kniga3 = Book(title = 'Полное собрание сочинений Иванова', id_publisher = 2)
@@ -32,24 +32,21 @@ def testing():
     session.add_all([knigi_v_dom, knigi_12345, kniga1, kniga2, kniga3, shop1, shop2, 
                     stock1, stock2, stock3, sale1, sale2, sale3])
     session.commit()
-    
-    q1 = session.query(Publisher).join(Book.publisher)
-    for pub in q1.all():
-        for b in pub.book:
-            sq1 = session.query(Stock).filter(Stock.id_book == b.id).subquery()
-            q2 = session.query(Sale).join(sq1, Sale.id_stock == sq1.c.id)
-            for s in q2.all():
-                print(b.title, ' - ', pub.name, ' - ', s.price, ' - ', s.date_sale)
 
-
-    # q2 = session.query(Shop).join(Stock.shop)
-    # # for sho in subq2.all():
-    # #     for sto in sho.stock:
-    # #         print(sho.id, sho.name, ' - ', sto.id, sto.count, 'шт.')
-    # # subq3 = subq1.join(subq2, subq1.c.id == subq2.c.id_book)
-
-    # subquery1 = q1.subquery()
-    # subquery2 = q2.subquery()
+def show_shops_by_publisher(publisher_name):
+    sq1 = session.query(Publisher).filter(Publisher.name == publisher_name).subquery()
+    q1 = session.query(Book).join(sq1, Book.id_publisher == sq1.c.id)
+    for b in q1.all():
+        q2 = session.query(Shop).join(Stock.shop).filter(Stock.id_book == b.id)
+        for sh in q2.all():
+            for st in sh.stock:
+                q3 = session.query(Sale).filter(Sale.id_stock == st.id)
+                for sl in q3.all():
+                    print(sl.price, sl.date_sale)
+                    str_book_title = '{:<30}'.format(b.title)
+                    str_shop_name = '{:<20}'.format(sh.name)
+                    str_price = '{:<6}'.format(str(sl.price))
+                    print(f'{str_book_title} | {str_shop_name} | {str_price} | {sl.date_sale}')
 
 #Подключение к БД, создание сессии
 db, user, password = enter_db_user_credentials()
@@ -63,7 +60,13 @@ delete_all_tables(engine)
 #создание таблиц
 create_tables(engine)
 
-testing()
+#Наполнение таблиц данными
+import_data_to_bd()
+
+publishers = ['Издатель 451','Книги в каждый дом!', 'Книги РАЗ, ДВА, ТРИ, ЧЕТЫРЕ, ПЯТЬ']
+for publisher in publishers:
+    show_shops_by_publisher(publisher)
+    print('-----')
 
 session.commit()
 session.close()
